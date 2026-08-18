13617f89639107079cdcfa93cf1c8873df65664e
GitHub Action <action@github.com>
2026-08-18 10:44:12 +0000

    Apply PHP CS Fixer fixes [skip ci]

--- demo/symfony8/config/bundles.php
+++ demo/symfony8/config/bundles.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle;
 use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
+use Nowo\HotReloadBundle\NowoHotReloadBundle;
 use Nowo\TagInputBundle\NowoTagInputBundle;
 use Nowo\TaskBoardBundle\TaskBoardBundle;
 use Nowo\TimeTrackBundle\TimeTrackBundle;
 use Nowo\TiptapEditorBundle\NowoTiptapEditorBundle;
-use Nowo\HotReloadBundle\NowoHotReloadBundle;
 use Nowo\TwigInspectorBundle\NowoTwigInspectorBundle;
 use Nowo\UiKitBundle\NowoUiKitBundle;
 use Symfony\Bundle\DebugBundle\DebugBundle;
@@ -29,7 +29,7 @@ return [
     TimeTrackBundle::class          => ['all' => true],
     TaskBoardBundle::class          => ['all' => true],
     NowoUiKitBundle::class          => ['all' => true],
-    NowoHotReloadBundle::class  => ['dev' => true, 'test' => true],
+    NowoHotReloadBundle::class      => ['dev' => true, 'test' => true],
     NowoTwigInspectorBundle::class  => ['dev' => true, 'test' => true],
     WebProfilerBundle::class        => ['dev' => true, 'test' => true],
     DebugBundle::class              => ['dev' => true],
